9439d471cb8093f23d0ee50ff8c1fcd82666f71e
itssuny <itssuny4@gmail.com>
2024-03-07 12:48:26 +0900

    first source(pollutant means)

---
+++ pollutantmean.R
@@ -0,0 +1,51 @@
+pollutantmean <- function(directory, pollutant, id = 1:332){
+  
+  print(R.version.string)
+  
+  setwd(directory)
+  
+  totalSum <- 0
+  count <- 0
+  for(ii in id){
+
+
+    if(ii<10 ){
+      prefix <- paste("00", ii, sep="")
+
+    }else if(ii>= 10 && ii<99){
+      prefix <- paste("0", ii, sep="")
+      
+    }else {
+      prefix <- paste(ii, sep="")
+      
+    }
+    fileName <- paste(prefix, ".", "csv", sep="") 
+  
+    ##print(fileName)
+    
+    ## read file
+    con <- file(fileName, "r")
+    data <- read.csv(con)
+    close(con)
+    
+    targetData <- data[[pollutant]]
+    ##print(targetData)
+    
+    y <- targetData[!is.na(targetData)]
+    ##print("yyyyyyyyyyyyyyyyyyy")
+    ##print(y)
+    ##print(sum(y))
+    totalSum <- totalSum + sum(y)
+    count <- count + length(y)
+    ##print(totalSum)
+    ##print(count)
+    
+  }
+
+  print(totalSum/count)
+
+ 
+
+
+  setwd("../")
+}
